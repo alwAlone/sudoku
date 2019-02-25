9e551448d943fe2c81347b2e56e1c563d4e42f6b
alwAlone <gidm.teacher@gmail.com>
2019-02-25 20:50:38 +0300

    feat: a lot of pain =)

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ module.exports = function solveSudoku(matrix) {
     }
     return result;
   }
+
+
   // функция ищет варанты значений
   function findOptions(positions, matrix) {
     // Составляет массив из элементов. Формирует столбец.
@@ -26,10 +28,44 @@ module.exports = function solveSudoku(matrix) {
       }
       return result;
     }
+
+
     // Составляет массив из квадрата 3х3
     function findSquare(matrix, pos) {
-      
+      var result = [],
+          row = pos[0],
+          col = pos[1],
+          sq_row = row/3,
+          sq_col = col/3,
+          opt = null;
+          
+      if ((sq_row >= 0) && (sq_row < 1)) {
+        for (var i = 0; i < 3; i++) {
+          for (var j = 0; j < 3; j++) {
+            opt = matrix[i][j];
+            result.push(opt);
+          }
+        }
+      } else if ((sq >= 1) && (sq < 2)) {
+        for (var i = 3; i < 6; i++) {
+          for (var j = 3; j < 6; j++) {
+            opt = matrix[i][j];
+            result.push(opt);
+          }
+        }
+      } else if ((sq >= 2) && (sq < 3)) {
+        for (var i = 6; i < 9; i++) {
+          for (var j = 6; j < 9; j++) {
+            opt = matrix[i][j];
+            result.push(opt);
+          }
+        }
+      }
+      console.log(result);
+      return result;
     }
+
+
     for (var i = 0; i < positions.length; i++) {
       var pos = positions[i],
           row = pos[0],
@@ -38,13 +74,13 @@ module.exports = function solveSudoku(matrix) {
 
       row = matrix[row];
       col = findCol(matrix, col);
-      console.log(col);
+      square = findSquare(matrix, pos);
     }
-    var result = positions;
+    var result = square;
     return result;
   }
+
   var positions = findZero(matrix),
       result = findOptions(positions, matrix);
-
   return result;
 }
