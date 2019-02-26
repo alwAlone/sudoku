1335303a475abfb6cd405992756c5ac23804376f
alwAlone <gidm.teacher@gmail.com>
2019-02-26 17:21:22 +0300

    feat: search all empty cells and write to array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function solveSudoku(matrix) {
-  
+  var result = [],
+      options = []
+      option = null;
+
+  for (var i = 0; i < 9; i++) {
+    for (var j = 0; j < 9; j++) {
+      option = matrix[i][j];
+      
+      if (option === 0) {
+        option = {
+          row: i,
+          col: j
+        };
+        options.push(option);
+      }
+    }
+  }
+  return result;
 }
